29e4e732e7fab3202329208f79fad702f2de4125
YousefBtech <143946659+YousefBtech@users.noreply.github.com>
2024-03-27 12:41:38 +0200

    change

--- Package.swift
+++ Package.swift
@@ -16,9 +16,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignSystemCore"),
+                    name: "DesignSystemCore",
+                    path: "DesignSystemCore/Sources"
+                ),
         .testTarget(
             name: "DesignSystemCoreTests",
-            dependencies: ["DesignSystemCore"]),
+            dependencies: ["DesignSystemCore"],
+            path: "DesignSystemCore/Tests"),
     ]
 )
