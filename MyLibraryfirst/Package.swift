580f1ffaa8c4d8a32a54b97c4be5f46244ab8087
YousefBtech <143946659+YousefBtech@users.noreply.github.com>
2024-11-03 17:37:34 +0200

    update

--- MyLibraryfirst/Package.swift
+++ MyLibraryfirst/Package.swift
@@ -1,24 +1,23 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "MyLibraryfirst",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "MyLibraryfirst",
+            targets: ["MyLibraryfirst"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary"),
+            name: "MyLibraryfirst"),
         .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]
-        ),
+            name: "MyLibraryfirstTests",
+            dependencies: ["MyLibraryfirst"]),
     ]
 )
